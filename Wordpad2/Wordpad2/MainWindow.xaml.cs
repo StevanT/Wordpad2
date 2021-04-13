6c8ead3685a9883f6729a043e443a7bb7a660aef
StevanT <evastnicle.stev@gmail.com>
2021-04-13 22:12:26 +0200

    Bugfix, odvajanje u drugu klasu, pocetak exporta

--- Wordpad2/Wordpad2/MainWindow.xaml.cs
+++ Wordpad2/Wordpad2/MainWindow.xaml.cs
@@ -8,16 +8,36 @@ using System.Windows.Input;
 using System.Windows.Media;
 using Microsoft.Win32;
 using System.Windows.Controls;
+using System.Windows.Navigation;
+using System.Diagnostics;
 
 namespace Wordpad2
 {
 	public partial class MainWindow : Window
 	{
-		public MainWindow()
+        public bool UseShellExecute { get; private set; }
+		OpenSaveExportClass openSaveExportClass = new OpenSaveExportClass();
+
+        public MainWindow()
 		{
 			InitializeComponent();
 			cmbFontFamily.ItemsSource = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
 			cmbFontSize.ItemsSource = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
+		}
+
+		private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			openSaveExportClass.Open_Executed(sender, e, rtbEditor);
+		}
+
+		private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
+		{
+			openSaveExportClass.Save_Executed(sender, e, rtbEditor);
+		}
+
+		private void Export_Executed(object sender, RoutedEventArgs e)
+		{
+			openSaveExportClass.Export_Executed(sender, e, rtbEditor);
 		}
 
 		private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
@@ -47,29 +67,7 @@ namespace Wordpad2
 			}
 		}
 
-		private void Open_Executed(object sender, ExecutedRoutedEventArgs e)
-		{
-			OpenFileDialog dlg = new OpenFileDialog();
-			dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
-			if (dlg.ShowDialog() == true)
-			{
-				FileStream fileStream = new FileStream(dlg.FileName, FileMode.Open);
-				TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
-				range.Load(fileStream, DataFormats.Rtf);
-			}
-		}
 
-		private void Save_Executed(object sender, ExecutedRoutedEventArgs e)
-		{
-			SaveFileDialog dlg = new SaveFileDialog();
-			dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
-			if (dlg.ShowDialog() == true)
-			{
-				FileStream fileStream = new FileStream(dlg.FileName, FileMode.Create);
-				TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
-				range.Save(fileStream, DataFormats.Rtf);
-			}
-		}
 
 		private void cmbFontFamily_SelectionChanged(object sender, SelectionChangedEventArgs e)
 		{
@@ -80,6 +78,7 @@ namespace Wordpad2
 		private void cmbFontSize_TextChanged(object sender, TextChangedEventArgs e)
 		{
 			if (cmbFontFamily.SelectedItem != null)
+				if(int.TryParse(cmbFontSize.Text, out int x)) 
 				rtbEditor.Selection.ApplyPropertyValue(TextElement.FontSizeProperty, cmbFontSize.Text);
 		}
 
@@ -100,5 +99,46 @@ namespace Wordpad2
 				cmbFontSize.Text = Convert.ToString(Convert.ToInt32(cmbFontSize.Text) - 1);
 			}
 		}
+
+        private void Button_Hyperlink(object sender, RoutedEventArgs e)
+        {
+			if (!rtbEditor.Selection.IsEmpty)
+			{
+				string inputRead = new InputBox("Enter hyperlink:", "Add hyperlink", "").ShowDialog();
+
+				Hyperlink hlink = new Hyperlink();
+				TextRange tr = new TextRange(rtbEditor.Selection.Start, rtbEditor.Selection.End);
+
+				try
+				{
+					hlink.NavigateUri = new Uri(inputRead);
+					hlink = new Hyperlink(tr.Start, tr.End);
+					hlink.NavigateUri = new Uri(inputRead);
+					hlink.RequestNavigate += Hyperlink_RequestNavigate;
+					hlink.IsEnabled = true;
+				} catch (Exception exception)
+				{
+					MessageBox.Show(exception.Message);
+					
+				}
+
+			}
+		}
+
+		private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+		{
+			try
+			{
+				Process pro = new Process();
+				UseShellExecute = true;
+				pro.StartInfo.FileName = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe";
+				pro.StartInfo.Arguments = e.Uri.AbsoluteUri;
+				pro.Start();
+				e.Handled = true;
+			} catch(Exception exception) { 
+				MessageBox.Show(exception.Message); }
+		}
+
+
     }
 }
---
+++ Wordpad2/Wordpad2/OpenSaveExportClass.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using Microsoft.Win32;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using System.Diagnostics;
+
+namespace Wordpad2
+{
+    internal class OpenSaveExportClass
+    {
+		public void Open_Executed(object sender, ExecutedRoutedEventArgs e, RichTextBox rtbEditor)
+		{
+			OpenFileDialog dlg = new OpenFileDialog();
+			dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+			if (dlg.ShowDialog() == true)
+			{
+				FileStream fileStream = new FileStream(dlg.FileName, FileMode.Open);
+				TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+				range.Load(fileStream, DataFormats.Rtf);
+			}
+		}
+
+		public void Save_Executed(object sender, ExecutedRoutedEventArgs e, RichTextBox rtbEditor)
+		{
+			SaveFileDialog dlg = new SaveFileDialog();
+			dlg.Filter = "Rich Text Format (*.rtf)|*.rtf|All files (*.*)|*.*";
+			if (dlg.ShowDialog() == true)
+			{
+				FileStream fileStream = new FileStream(dlg.FileName, FileMode.Create);
+				TextRange range = new TextRange(rtbEditor.Document.ContentStart, rtbEditor.Document.ContentEnd);
+				range.Save(fileStream, DataFormats.Rtf);
+			}
+		}
+
+		public void Export_Executed(object sender, RoutedEventArgs e, RichTextBox rtbEditor)
+        {
+
+        }
+	}
+}
